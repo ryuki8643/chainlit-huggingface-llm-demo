eb830fa8f89b146f228cb6361fb277ddbeb2b42d
ryuki8643 <garyuki88me@gmail.com>
2023-06-08 01:49:57 +0900

    define model name

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ import chainlit as cl
 
 import models.facebook_blenderbot_400m as model
 
+
+@cl.on_chat_start
+def main():
+
+    cl.Message(
+        content=f"My name is {model.name()}",
+    ).send()
+
 @cl.on_message
 def main(message: str):
 
     model_output_message=model.chat_message(message)
-    
+
     cl.Message(
         content=model_output_message,
     ).send()
